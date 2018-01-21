d7bd7b758926ba0446a63061409607150ccca74d
Dietrich Miller <oddacon@live.com>
2018-01-20 23:38:04 -0600

    Create ColorSwirl.ino
    this is the main file

---
+++ MyBlinkySketch/ColorSwirl.ino
@@ -0,0 +1,189 @@
+#include <FastLED.h>
+
+#include "FastLED_RGBW.h"     // Include the FastLED_RGBW helper file 
+
+#define LED_COUNT 60
+
+#define LED_OUT       13
+#define BUTTON_IN     10
+#define ANALOG_INPUT  A9
+#define EXTRA_PIN_A    7
+#define EXTRA_PIN_B   11
+
+CRGBW leds[LED_COUNT];
+CRGB *ledsRGB = (CRGB *) &leds[0];
+
+#define LED_OUT       13
+#define BUTTON_IN     10
+#define ANALOG_INPUT  A9
+#define EXTRA_PIN_A    7
+#define EXTRA_PIN_B   11
+
+#define BRIGHT_STEP_COUNT 8
+#define STARTING_BRIGHTNESS 4
+volatile uint8_t brightnesSteps[BRIGHT_STEP_COUNT] = {5,15,40,70,93, 70, 40, 15};
+
+uint8_t brightness = 4;
+uint8_t lastBrightness = 4;
+
+// For fading in a new sketch
+long lastTime;
+
+float fadeIndex;
+#define FADE_STEPS 50
+
+// Button interrupt variables and Interrupt Service Routine
+uint8_t buttonState = 0;
+bool buttonDebounced;
+long buttonDownTime = 0;
+long buttonPressTime = 0;
+
+#define BUTTON_BRIGHTNESS_SWITCH_TIME  1     // Time to hold the button down to switch brightness
+#define BUTTON_PATTERN_SWITCH_TIME    1000   // Time to hold the button down to switch patterns
+
+
+void setup()
+{  
+  Serial.begin(57600);
+  
+    LEDS.addLeds<WS2812B, LED_OUT, RGB>(ledsRGB, getRGBWsize(LED_COUNT));
+  
+  brightness = STARTING_BRIGHTNESS;
+  LEDS.setBrightness(brightnesSteps[brightness]);
+  LEDS.show();
+
+  pinMode(BUTTON_IN, INPUT_PULLUP);
+  pinMode(ANALOG_INPUT, INPUT_PULLUP);
+  pinMode(EXTRA_PIN_A, INPUT_PULLUP);
+  pinMode(EXTRA_PIN_B, INPUT_PULLUP);
+  
+  // Interrupt set-up; see Atmega32u4 datasheet section 11
+  PCIFR  |= (1 << PCIF0);  // Just in case, clear interrupt flag
+  PCMSK0 |= (1 << PCINT6); // Set interrupt mask to the button pin (PCINT6)
+  PCICR  |= (1 << PCIE0);  // Enable interrupt
+}
+
+
+void colorLoop() {  
+  static uint8_t i = 0;
+  static int j = 0;
+  static int f = 0;
+  static int k = 0;
+  
+  for (uint8_t i = 0; i < LED_COUNT; i++) {
+    leds[i].r = 64*(1+sin(i/2.0 + j/4.0       ));
+    leds[i].g = 64*(1+sin(i/1.0 + f/9.0  + 2.1));
+    leds[i].b = 64*(1+sin(i/3.0 + k/14.0 + 4.2));
+  }
+  LEDS.show();
+  
+  j = j + 1;
+  f = f + 1;
+  k = k + 2;
+}
+
+void rainbowLoop(){
+  static uint8_t hue;
+
+for (uint8_t i = 0; i < LED_COUNT; i++) {
+    leds[i] = CHSV((i * 256 / LED_COUNT) + hue, 255, 255);
+  }
+  LEDS.show();
+  hue++;
+}
+
+void serialLoop() {
+  static int pixelIndex;
+  
+  unsigned long lastReceiveTime = millis();
+
+  while(true) {
+
+    if(Serial.available() > 2) {
+      lastReceiveTime = millis();
+
+      uint8_t buffer[3]; // Buffer to store three incoming bytes used to compile a single LED color
+
+      for (uint8_t x=0; x<3; x++) { // Read three incoming bytes
+        uint8_t c = Serial.read();
+        
+        if (c < 255) {
+          buffer[x] = c; // Using 255 as a latch semaphore
+        }
+        else {
+          LEDS.show();
+          pixelIndex = 0;
+          break;
+        }
+
+        if (x == 3) {   // If we received four serial bytes
+          if(pixelIndex == LED_COUNT) break; // Prevent overflow by ignoring the pixel data beyond LED_COUNT
+          leds[pixelIndex] = CRGBW(buffer[0], buffer[1], buffer[2], buffer[3]);  //use the FastLED CRGW structure instead of RGB & include another buffer for the color space
+          pixelIndex++;
+        }
+      }
+    }
+    
+    // If we haven't received data in 4 seconds, return to playing back our animation
+    if(millis() > lastReceiveTime + 4000) {
+      // TODO: Somehow the serial port gets trashed here, how to reset it?
+      return;
+    }
+  }
+}
+
+void loop()
+{
+  // If'n we get some data, switch to passthrough mode
+  if(Serial.available() > 0) {
+    serialLoop();
+  }
+  
+  // Draw the current pattern
+  colorLoop();
+  //rainbowLoop();
+  // Initial fade-in effect for pattern
+  if((millis() - lastTime > 15) && fadeIndex < FADE_STEPS) {
+    lastTime = millis();
+    fadeIndex++;
+    
+    LEDS.setBrightness(brightnesSteps[brightness]*(fadeIndex/FADE_STEPS));
+  }
+
+  LEDS.show();
+}
+
+
+// Called when the button is both pressed and released.
+ISR(PCINT0_vect){
+  buttonState = !(PINB & (1 << PINB6)); // Reading state of the PB6 (remember that HIGH == released)
+  
+  if(buttonState) {
+    // On button down, record the time so we can convert this into a gesture later
+    buttonDownTime = millis();
+    buttonDebounced = false;
+    
+    // And configure and start timer4 interrupt.
+    TCCR4B = 0x0F; // Slowest prescaler
+    TCCR4D = _BV(WGM41) | _BV(WGM40);  // Fast PWM mode
+    OCR4C = 0x10;        // some random percentage of the clock
+    TCNT4 = 0;  // Reset the counter
+    TIMSK4 = _BV(TOV4);  // turn on the interrupt
+    
+  }
+  else {
+    TIMSK4 = 0;  // turn off the interrupt
+  }
+}
+
+// This is called every xx ms while the button is being held down; it counts down then displays a
+// visual cue and changes the pattern.
+ISR(TIMER4_OVF_vect) {
+  // If the user is still holding down the button after the first cycle, they were serious about it.
+  if(buttonDebounced == false) {
+    buttonDebounced = true;
+    lastBrightness = brightness;
+    brightness = (brightness + 1) % BRIGHT_STEP_COUNT;
+    LEDS.setBrightness(brightnesSteps[brightness]);
+  }
+}
